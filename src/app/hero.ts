0f3da93e4dc512cc76d3498212153acfb01a5334
nathan <nathankfoss@gmail.com>
2019-12-22 21:33:25 -0600

    Create heroes component with editable name field (two-way binding)

---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+export class Hero {
+    id: number;
+    name: string;
+    faction: string;
+    class: string;
+    specialty: string;
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = "Alkin"
+  hero: Hero = {
+    id: 1,
+    name: "Alkin",
+    faction: "Fortress",
+    class: "Beastmaster",
+    specialty: "Gorgons"
+  };
 
   constructor() { }
 
