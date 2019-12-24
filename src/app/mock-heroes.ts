76013730443f5b06c11ab270bedf50fa5e0f88bb
nathan <nathankfoss@gmail.com>
2019-12-24 00:59:08 -0600

    Implement hero list and interface

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Gunnar', faction: 'Dungeon', class: 'Overlord', specialty: 'Logistics'},
+    { id: 12, name: 'Luna', faction: 'Conflux', class: 'Elementalist', specialty: 'Fire Wall'},
+    { id: 13, name: 'Solmyr', faction: 'Tower', class: 'Wizard', specialty: 'Chain Lightning'},
+    { id: 14, name: 'Tazar', faction: 'Rampart', class: 'Ranger', specialty: 'Armorer'},
+    { id: 15, name: 'Pyre', faction: 'Inferno', class: 'Heretic', specialty: 'Ballista'}
+]
