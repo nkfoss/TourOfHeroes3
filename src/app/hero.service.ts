6478c398227ed7e18f89684c9fffa8a81aaa8368
nathan <nathankfoss@gmail.com>
2019-12-24 22:46:35 -0600

    Implement message service for communication between components

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero'
-import { HEROES } from './mock-heroes'
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of} from 'rxjs'
+import { MessageService } from './message.service'
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _AFTER_ fetchiing the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
 }
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
-  selector: 'app-message',
+  selector: 'app-messages',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public messageService: MessageService) { }
 
   ngOnInit() {
   }
